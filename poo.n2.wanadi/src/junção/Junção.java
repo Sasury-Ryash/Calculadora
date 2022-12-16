ef947f8e1376441e0b6ebdcd520817907209196a
Ana Carolina <111385282+Sasury-Ryash@users.noreply.github.com>
2022-12-16 14:16:47 -0300

    Add files via upload

---
+++ poo.n2.wanadi/src/junção/Junção.java
@@ -0,0 +1,5 @@
+package junção;
+
+public class Junção {
+
+}
---
+++ poo.n2.wanadi/src/tela/de/login/LoginController.java
@@ -0,0 +1,28 @@
+package tela.de.login;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author atila
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private void exit (ActionEvent event){
+        System.exit(0);
+    }
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
